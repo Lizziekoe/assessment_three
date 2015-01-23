25ec2afc3f861c8678e6eca46f2fbb41569e6988
Lizzie <lizziefkoehler@gmail.com>
2015-01-23 09:46:05 -0800

    create a Stylist class

---
+++ spec/stylist_spec.rb
@@ -0,0 +1,62 @@
+require('spec_helper')
+
+describe(Stylist) do
+  describe(".all") do
+    it("starts off with no stylists") do
+      expect(Stylist.all()).to(eq([]))
+    end
+  end
+
+  describe('#name') do
+    it('tells you its name') do
+      stylist = Stylist.new({:name => "Aveda", :id => nil})
+      expect(stylist.name()).to(eq("Aveda"))
+    end
+  end
+
+  describe('#id') do
+    it('sets its ID when you save it') do
+      stylist = Stylist.new({:name => "Aveda", :id => nil})
+      stylist.save()
+      expect(stylist.id()).to(be_an_instance_of(Fixnum))
+    end
+  end
+
+  describe('#save') do
+    it('lets you save stylists to the database') do
+      stylist = Stylist.new({:name => "Aveda", :id => nil})
+      stylist.save()
+      expect(Stylist.all()).to(eq([stylist]))
+    end
+  end
+
+#   describe(".find") do
+#     it("returns a stylist by its ID number") do
+#       test_stylist = Stylist.new({:name => "Aveda", :id => nil})
+#       test_stylist.save()
+#       test_stylist2 = Stylist.new({:name => "Home stuff", :id => nil})
+#       test_stylist2.save()
+#       expect(Stylist.find(test_stylist2.id())).to(eq(test_stylist2))
+#     end
+#   end
+
+#   describe("#clients") do
+#     it("returns an array of clients for that stylist") do
+#       test_stylist = Stylist.new({:name => "Aveda", :id => nil})
+#       test_stylist.save()
+#       test_client = Client.new({:name => "Ruby", :Stylist_id => test_Stylist.id()})
+#       test_client.save()
+#       test_client2 = Client.new({:name => "Julia", :Stylist_id => test_Stylist.id()})
+#       test_client2.save()
+#       expect(test_stylist.clients()).to(eq([test_client, test_client2]))
+#     end
+#   end
+
+  describe('#==') do
+    it('is the same Stylist if it has the same name') do
+      stylist1 = Stylist.new({:name => "Aveda"})
+      stylist2 = Stylist.new({:name => "Aveda"})
+      expect(stylist1).to(eq(stylist2))
+    end
+  end
+end
